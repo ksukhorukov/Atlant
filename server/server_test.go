136b32484da09df40b4edf36852d77df8054b399
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2021-04-22 09:15:05 +0100

    TestMongoAddress

--- server/server_test.go
+++ server/server_test.go
@@ -8,3 +8,16 @@ import (
 func TestGetCursorIndex(t *testing.T) {
 	fmt.Println("GetCursorIndex test")
 }
+
+func TestMongoAddress(t *testing.T) {
+	mongo_address = "127.0.0.1"
+	mongo_port = 27017
+
+	mongo_url := fmt.Sprintf("mongodb://%s:%d", mongo_address, mongo_port)
+	
+	func_result := MongoAddress()
+
+	if(func_result != mongo_url) {
+		t.Errorf("%s != %s\n", func_result, mongo_url)
+	}
+}
